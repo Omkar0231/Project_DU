cf4e679106336e685d710a4502e5926465ace47b
Omkar0231 <omkar.thouta0231@gmail.com>
2018-10-15 11:19:51 +0530

    Omkar made the voice Modulation for Blind Persons.(Only the startup Screen)

--- Project_DU/app/src/main/java/com/example/omkar/project_du/HansrajActivity2.java
+++ Project_DU/app/src/main/java/com/example/omkar/project_du/HansrajActivity2.java
@@ -1,10 +1,21 @@
 package com.example.omkar.project_du;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -19,23 +30,96 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Locale;
 
 
 // This is the first activity
 public class HansrajActivity2 extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener , View.OnClickListener{
+        implements NavigationView.OnNavigationItemSelectedListener , View.OnClickListener {
 
-    ImageButton learnmoreHannsraj,learnmoreRamjas;
+    ImageButton learnmoreHannsraj, learnmoreRamjas;
     ImageView du_colleges;
+    MediaPlayer AudioForBlind;   // Are you a blind person? & yes or no (audio)
+    String Speechflag="";
 
+    SpeechRecognizer mSpeechRecognizer;
+    Intent mRecognizerIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hansraj2);
 
+        checkPermission(); // for checking the permissions
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+        mRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        mRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        mSpeechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+
+                ArrayList<String> mSpeechToText = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                if(mSpeechToText!=null){
+                    if(mSpeechToText.get(0).equals("yes")){
+                        Speechflag="yes";
+                    }
+
+                   else if(mSpeechToText.get(0).equals("no")){
+                        Speechflag="no";
+                    }
+
+                }
+
+
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -62,13 +146,20 @@ public class HansrajActivity2 extends AppCompatActivity
         learnmoreRamjas = findViewById(R.id.readmoreRamj);
         du_colleges = findViewById(R.id.ducollegesid);
 
+        //Audio For Blind
+        AudioForBlind = MediaPlayer.create(HansrajActivity2.this, R.raw.areyouablindperson);
 
         learnmoreHannsraj.setOnClickListener(this);
 
 
-        AlertDialog.Builder builder=new AlertDialog.Builder(this);
-        builder.setMessage("Are you a Blind Person ?")
+        final AlertDialog dialog;
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+
+        alert.setMessage("Are you a Blind Person ?")
                 .setCancelable(false)
+
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -82,74 +173,125 @@ public class HansrajActivity2 extends AppCompatActivity
                         dialog.cancel();
                     }
                 });
-        AlertDialog alertDialog=builder.create();
-        alertDialog.show();
+        dialog=alert.create();
+        dialog.show();        // Show DialogueBox
+        AudioForBlind.start();     // Play Audio
+
+        //Executing after 3 seconds
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                   mSpeechRecognizer.startListening(mRecognizerIntent);
+
+             }
+        }, 3000);
+
+
+        //Executing after 7 seconds
+        final Handler handler1 = new Handler();
+        handler1.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                // If not Blind, the dialogBox closes
+                if(Speechflag.equals("no")){
+                    dialog.cancel();
+                }
+
+                // If Blind, Another Activity Starts
+                else if(Speechflag.equals("yes")){
+                   Intent BlindHansrajPage = new Intent(HansrajActivity2.this,HansrajPageBlind.class);
+                   startActivity(BlindHansrajPage);
+                }
+
+
+            }
+        }, 7000);
+
+
 
     }
 
-    @Override
-    public void onClick(View v) {
-        if(v.getId()==R.id.readmoreH){
-            Intent Hansraj=new Intent(HansrajActivity2.this,HansrajActivity.class);
-            startActivity(Hansraj);
+
+        // Permission Check for Record Audio
+        private void checkPermission() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED)) {
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.parse("package:" + getPackageName()));
+                    startActivity(intent);
+                    finish();
+                }
+            }
         }
-    }
 
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+            @Override
+        public void onClick (View v){
+            if (v.getId() == R.id.readmoreH) {
+                Intent Hansraj = new Intent(HansrajActivity2.this, HansrajActivity.class);
+                startActivity(Hansraj);
+            }
         }
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.hansraj_activity2, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        @Override
+        public void onBackPressed () {
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                super.onBackPressed();
+            }
+        }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        @Override
+        public boolean onCreateOptionsMenu (Menu menu){
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.hansraj_activity2, menu);
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        @Override
+        public boolean onOptionsItemSelected (MenuItem item){
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
+            }
 
-        if (id == R.id.searchwmarks) {
-            // Handle the camera action
-        } else if (id == R.id.course) {
-
-        } else if (id == R.id.marks) {
-
-        } else if (id == R.id.documents) {
-
-        } else if (id == R.id.dupros) {
-
-        } else if (id == R.id.nav_send) {
-
+            return super.onOptionsItemSelected(item);
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+        @SuppressWarnings("StatementWithEmptyBody")
+        @Override
+        public boolean onNavigationItemSelected (MenuItem item){
+            // Handle navigation view item clicks here.
+            int id = item.getItemId();
+
+            if (id == R.id.searchwmarks) {
+                // Handle the camera action
+            } else if (id == R.id.course) {
+
+            } else if (id == R.id.marks) {
+
+            } else if (id == R.id.documents) {
+
+            } else if (id == R.id.dupros) {
+
+            } else if (id == R.id.nav_send) {
+
+            }
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
     }
-}
+
