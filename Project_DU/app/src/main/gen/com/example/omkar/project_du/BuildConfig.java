1873a799399f8b2d372a428a0ba199aaeec82fb1
Omkar0231 <omkar.thouta0231@gmail.com>
2018-11-05 18:15:45 +0530

    "omkar Changed the manifest file 1st time"

---
+++ Project_DU/app/src/main/gen/com/example/omkar/project_du/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.omkar.project_du;
+
+/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
+public final class BuildConfig {
+  public final static boolean DEBUG = Boolean.parseBoolean(null);
+}
